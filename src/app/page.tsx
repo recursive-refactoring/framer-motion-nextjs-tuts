aed77b3a4ebd24a907f02cce49aa329911bed50f
unknown <noumanishtiaq53@gmail.com>
2025-04-13 10:45:37 +0500

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import { BulletsListCard } from "@/components/cards/bullets-list-card";
 import { HeroCard } from "@/components/cards/hero-card";
 import { ListCard } from "@/components/cards/list-card";
+import { Fragment } from "react";
 
 export default function Home() {
   return (
@@ -9,7 +11,156 @@ export default function Home() {
         info="Motion frmaer is aathe aminaiton aod"
       /> */}
       <HeroCard />
-      <ListCard />
+      {[
+        {
+          _id: 1,
+          heading: "Seamless Integrationa MaVailabel",
+          description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic sapiente illo consequuntur dolore dolores eligendi provident dolorem cupiditate laboriosam voluptatum!`,
+          itemsLists: [
+            {
+              text: "Lorem ipsum, dolor sit amet",
+            },
+            {
+              text: "dolorem cupiditate laboriosam voluptatum!",
+            },
+            {
+              text: "consequuntur dolore dolores eligendi provident",
+            },
+          ],
+        },
+        {
+          _id: 2,
+          heading: "Seamless Integrationa MaVailabel",
+          description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic sapiente illo consequuntur dolore dolores eligendi provident dolorem cupiditate laboriosam voluptatum!`,
+          itemsLists: [
+            {
+              text: "Lorem ipsum, dolor sit amet",
+            },
+            {
+              text: "dolorem cupiditate laboriosam voluptatum!",
+            },
+            {
+              text: "consequuntur dolore dolores eligendi provident",
+            },
+          ],
+        },
+        {
+          _id: 3,
+          heading: "Seamless Integrationa MaVailabel",
+          description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic sapiente illo consequuntur dolore dolores eligendi provident dolorem cupiditate laboriosam voluptatum!`,
+          itemsLists: [
+            {
+              text: "Lorem ipsum, dolor sit amet",
+            },
+            {
+              text: "dolorem cupiditate laboriosam voluptatum!",
+            },
+            {
+              text: "consequuntur dolore dolores eligendi provident",
+            },
+          ],
+        },
+      ]?.map((list: any) => (
+        <Fragment key={list?._id}>
+          <ListCard />
+        </Fragment>
+      ))}
+      {[
+        {
+          _id: 1,
+          itemsList: [
+            {
+              heading: "Customizable & Social Sol",
+              description:
+                "Wheathe you'reLorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque sunt necessitatibus, quas iure laborum nobis explicabo! Qui officia ad aut porro quam veritatis maxime, dolorum deleniti ipsam assumenda quasi debitis sint minima? Voluptatem enim doloremque quibusdam, dolor necessitatibus nobis inventore. ",
+            },
+            {
+              heading: "Customizable & Social Sol",
+              description:
+                "Wheathe you'reLorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque sunt necessitatibus, quas iure laborum nobis explicabo! Qui officia ad aut porro quam veritatis maxime, dolorum deleniti ipsam assumenda quasi debitis sint minima? Voluptatem enim doloremque quibusdam, dolor necessitatibus nobis inventore. ",
+            },
+            {
+              heading: "Customizable & Social Sol",
+              description:
+                "Wheathe you'reLorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque sunt necessitatibus, quas iure laborum nobis explicabo! Qui officia ad aut porro quam veritatis maxime, dolorum deleniti ipsam assumenda quasi debitis sint minima? Voluptatem enim doloremque quibusdam, dolor necessitatibus nobis inventore. ",
+            },
+            {
+              heading: "Customizable & Social Sol",
+              description:
+                "Wheathe you'reLorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque sunt necessitatibus, quas iure laborum nobis explicabo! Qui officia ad aut porro quam veritatis maxime, dolorum deleniti ipsam assumenda quasi debitis sint minima? Voluptatem enim doloremque quibusdam, dolor necessitatibus nobis inventore. ",
+            },
+            {
+              heading: "Customizable & Social Sol",
+              description:
+                "Wheathe you'reLorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque sunt necessitatibus, quas iure laborum nobis explicabo! Qui officia ad aut porro quam veritatis maxime, dolorum deleniti ipsam assumenda quasi debitis sint minima? Voluptatem enim doloremque quibusdam, dolor necessitatibus nobis inventore. ",
+            },
+          ],
+        },
+        {
+          _id: 2,
+          itemsList: [
+            {
+              heading: "Customizable & Social Sol",
+              description:
+                "Wheathe you'reLorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque sunt necessitatibus, quas iure laborum nobis explicabo! Qui officia ad aut porro quam veritatis maxime, dolorum deleniti ipsam assumenda quasi debitis sint minima? Voluptatem enim doloremque quibusdam, dolor necessitatibus nobis inventore. ",
+            },
+            {
+              heading: "Customizable & Social Sol",
+              description:
+                "Wheathe you'reLorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque sunt necessitatibus, quas iure laborum nobis explicabo! Qui officia ad aut porro quam veritatis maxime, dolorum deleniti ipsam assumenda quasi debitis sint minima? Voluptatem enim doloremque quibusdam, dolor necessitatibus nobis inventore. ",
+            },
+            {
+              heading: "Customizable & Social Sol",
+              description:
+                "Wheathe you'reLorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque sunt necessitatibus, quas iure laborum nobis explicabo! Qui officia ad aut porro quam veritatis maxime, dolorum deleniti ipsam assumenda quasi debitis sint minima? Voluptatem enim doloremque quibusdam, dolor necessitatibus nobis inventore. ",
+            },
+            {
+              heading: "Customizable & Social Sol",
+              description:
+                "Wheathe you'reLorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque sunt necessitatibus, quas iure laborum nobis explicabo! Qui officia ad aut porro quam veritatis maxime, dolorum deleniti ipsam assumenda quasi debitis sint minima? Voluptatem enim doloremque quibusdam, dolor necessitatibus nobis inventore. ",
+            },
+            {
+              heading: "Customizable & Social Sol",
+              description:
+                "Wheathe you'reLorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque sunt necessitatibus, quas iure laborum nobis explicabo! Qui officia ad aut porro quam veritatis maxime, dolorum deleniti ipsam assumenda quasi debitis sint minima? Voluptatem enim doloremque quibusdam, dolor necessitatibus nobis inventore. ",
+            },
+          ],
+        },
+        {
+          _id: 3,
+          itemsList: [
+            {
+              heading: "Customizable & Social Sol",
+              description:
+                "Wheathe you'reLorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque sunt necessitatibus, quas iure laborum nobis explicabo! Qui officia ad aut porro quam veritatis maxime, dolorum deleniti ipsam assumenda quasi debitis sint minima? Voluptatem enim doloremque quibusdam, dolor necessitatibus nobis inventore. ",
+            },
+            {
+              heading: "Customizable & Social Sol",
+              description:
+                "Wheathe you'reLorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque sunt necessitatibus, quas iure laborum nobis explicabo! Qui officia ad aut porro quam veritatis maxime, dolorum deleniti ipsam assumenda quasi debitis sint minima? Voluptatem enim doloremque quibusdam, dolor necessitatibus nobis inventore. ",
+            },
+            {
+              heading: "Customizable & Social Sol",
+              description:
+                "Wheathe you'reLorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque sunt necessitatibus, quas iure laborum nobis explicabo! Qui officia ad aut porro quam veritatis maxime, dolorum deleniti ipsam assumenda quasi debitis sint minima? Voluptatem enim doloremque quibusdam, dolor necessitatibus nobis inventore. ",
+            },
+            {
+              heading: "Customizable & Social Sol",
+              description:
+                "Wheathe you'reLorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque sunt necessitatibus, quas iure laborum nobis explicabo! Qui officia ad aut porro quam veritatis maxime, dolorum deleniti ipsam assumenda quasi debitis sint minima? Voluptatem enim doloremque quibusdam, dolor necessitatibus nobis inventore. ",
+            },
+            {
+              heading: "Customizable & Social Sol",
+              description:
+                "Wheathe you'reLorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque sunt necessitatibus, quas iure laborum nobis explicabo! Qui officia ad aut porro quam veritatis maxime, dolorum deleniti ipsam assumenda quasi debitis sint minima? Voluptatem enim doloremque quibusdam, dolor necessitatibus nobis inventore. ",
+            },
+          ],
+        },
+      ]?.map((list: any) => (
+        <Fragment key={list?._id}>
+          <BulletsListCard />
+        </Fragment>
+      ))}
     </>
   );
 }
---
+++ src/components/cards/bullets-list-card/index.tsx
@@ -0,0 +1,66 @@
+import { CommonCardBgManImage } from "@/assets/images/common/card";
+import { ContainerGridLayout } from "@/components/layouts/container-grid-layout";
+import { ItemGridLayout } from "@/components/layouts/item-grid-layout";
+import { BodyText } from "@/components/text/body-text";
+import { SubHeadingText } from "@/components/text/sub-heading-text";
+import { StarBorder } from "@mui/icons-material";
+import { Avatar } from "@mui/material";
+
+export const BulletsListCard = (props: any) => {
+  const {
+    itemsList = [
+      {
+        heading: "Customizable & Social Sol",
+        description:
+          "Wheathe you'reLorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque sunt necessitatibus, quas iure laborum nobis explicabo! Qui officia ad aut porro quam veritatis maxime, dolorum deleniti ipsam assumenda quasi debitis sint minima? Voluptatem enim doloremque quibusdam, dolor necessitatibus nobis inventore. ",
+      },
+      {
+        heading: "Customizable & Social Sol",
+        description:
+          "Wheathe you'reLorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque sunt necessitatibus, quas iure laborum nobis explicabo! Qui officia ad aut porro quam veritatis maxime, dolorum deleniti ipsam assumenda quasi debitis sint minima? Voluptatem enim doloremque quibusdam, dolor necessitatibus nobis inventore. ",
+      },
+      {
+        heading: "Customizable & Social Sol",
+        description:
+          "Wheathe you'reLorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque sunt necessitatibus, quas iure laborum nobis explicabo! Qui officia ad aut porro quam veritatis maxime, dolorum deleniti ipsam assumenda quasi debitis sint minima? Voluptatem enim doloremque quibusdam, dolor necessitatibus nobis inventore. ",
+      },
+      {
+        heading: "Customizable & Social Sol",
+        description:
+          "Wheathe you'reLorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque sunt necessitatibus, quas iure laborum nobis explicabo! Qui officia ad aut porro quam veritatis maxime, dolorum deleniti ipsam assumenda quasi debitis sint minima? Voluptatem enim doloremque quibusdam, dolor necessitatibus nobis inventore. ",
+      },
+      {
+        heading: "Customizable & Social Sol",
+        description:
+          "Wheathe you'reLorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque sunt necessitatibus, quas iure laborum nobis explicabo! Qui officia ad aut porro quam veritatis maxime, dolorum deleniti ipsam assumenda quasi debitis sint minima? Voluptatem enim doloremque quibusdam, dolor necessitatibus nobis inventore. ",
+      },
+    ],
+  } = props;
+  return (
+    <ContainerGridLayout spacing={4} customStyles={{ padding: "1rem" }}>
+      <ItemGridLayout md={6}>
+        <Avatar
+          src={CommonCardBgManImage?.src}
+          alt="122"
+          variant="square"
+          sx={{ width: "100%", height: "100%", aspectRatio: "4/3" }}
+        />
+      </ItemGridLayout>
+      <ItemGridLayout md={6}>
+        <Avatar sx={{ width: 50, height: 50 }}>
+          <StarBorder sx={{ color: "text.primary" }} />
+        </Avatar>
+        <ul>
+          {itemsList.map((list: any) => (
+            <li style={{}}>
+              <SubHeadingText component="span">
+                {list?.heading ? `${list?.heading} :` : ""}
+              </SubHeadingText>{" "}
+              <BodyText component="span">{list?.description}</BodyText>
+            </li>
+          ))}
+        </ul>
+      </ItemGridLayout>
+    </ContainerGridLayout>
+  );
+};
